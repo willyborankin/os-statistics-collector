9f7f22959b4f97ad1b1f1bcaa921a0f57f44c2a8
Andrey Pleskach <andrey.v.pleskach@gmail.com>
2019-12-08 15:05:18 +0100

    opertaion system database for flyway

---
+++ consumer/conf/db/migration/os-metrics/V1__database_init.sql
@@ -0,0 +1,28 @@
+CREATE SCHEMA IF NOT EXISTS os_statistics;
+SET search_path TO os_statistics;
+
+CREATE TABLE host_hardware (
+    id                         UUID         NOT NULL,
+    host                       VARCHAR(100) NOT NULL,
+    ip_address                 INET         NULL,
+    cpu_utilization            BIGINT       NOT NULL,
+    system_load_average        BIGINT       NOT NULL,
+    free_physical_memory_size  BIGINT       NOT NULL,
+    total_physical_memory_size BIGINT       NOT NULL,
+    free_swap_space_size       BIGINT       NOT NULL,
+    total_swap_space_size      BIGINT       NOT NULL,
+    created_at                 TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) WITHOUT OIDS;
+
+CREATE TABLE host_file_system (
+    id           UUID         NOT NULL,
+    host         VARCHAR(100) NOT NULL,
+    ip_address   INET         NULL,
+    path         VARCHAR(50)  NOT NULL,
+    total_space  BIGINT       NOT NULL,
+    usable_space BIGINT       NOT NULL,
+    free_space   BIGINT       NOT NULL,
+    created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) WITHOUT OIDS;
